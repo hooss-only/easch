54abeca5143fdf6898b24575711fa2358a8b45d2
hooss-only <hoossonly@gmail.com>
2024-01-01 15:41:32 +0900

    Added pin setup

--- assets/scripts/startup.js
+++ assets/scripts/startup.js
@@ -1,10 +1,16 @@
 // if its first startup, make initial datas
 function isNotSetted(result) {
-	return typeof result.key == 'undefined';
+	return typeof result == 'undefined';
 }
 
 chrome.storage.sync.get(['search_url'], (result) => {
-	if (isNotSetted(result)) {
+	if (isNotSetted(result.search_url)) {
 		chrome.storage.sync.set({search_url: 'http://www.google.com/search?q=%s'});
+	}
+});
+
+chrome.storage.sync.get(['pins'], (result) => {
+	if (isNotSetted(result.pins)) {
+		chrome.storage.sync.set({pins: [{}, {}, {}, {}, {}]});
 	}
 });
