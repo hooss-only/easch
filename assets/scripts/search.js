b82007e3a8148b2ff8b5872a43120f9d1814942b
hooss-only <hoossonly@gmail.com>
2024-01-02 18:51:44 +0900

    Added the way to go to setting

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -4,8 +4,11 @@ let searchUrl;
 chrome.storage.sync.get(['search_url'], (result) => searchUrl = result.search_url);
 
 searchInput.addEventListener("keypress", (e) => {
-	if (e.key == "Enter") { 
-		console.log(searchUrl);
+	if (e.key == "Enter") {		
+		if (searchInput.value === "settings") {
+			window.location.href = "../settings/index.html";
+			return;
+		}
 
 		window.location.href = searchUrl.replace("%s", searchInput.value);
 	}
